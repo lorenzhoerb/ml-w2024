f40afd79b079243aed5084fa93c664c2d3868c26
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-11-29 14:31:55 +0100

    add exercise 2

---
+++ ex2/bootstrapper.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def bootstrap(data_set: pd.DataFrame) -> pd.DataFrame:
+    pass
---
+++ ex2/decision_tree.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class DecisionTree:
+    def __init__(self, max_depth: int = None):
+        pass
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        pass
+    def predict(self, X: pd.DataFrame) -> pd.Series:
+        pass
